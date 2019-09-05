8bd32489916ce9beaedf6c435a301bf05288dc90
Meghan Adkins <meghandadkins@gmail.com>
2019-09-05 11:12:46 -0400

    tweaked styles

--- src/styles/Styles.js
+++ src/styles/Styles.js
@@ -2,19 +2,22 @@ import styled from 'styled-components';
 
 export const CalculatorStyles = styled.div`
   background-color: #3772FF;
+  margin-top: 5px;
+  padding 0 5px 0 5px;
+  border-radius: 20px;
   width: 100%;
-  min-height: 100vh;
+  min-height: 80vh;
   display: grid;
   justify-items: center;
   grid-template-rows: minmax(200px 350px) 1fr;
   grid-template-columns: 1fr;
   @media (max-width: 500px) {
-    max-width: 90%;
+    margin-top: 40px;
+    max-width: 100%;
+    max-height: 95%;
     padding: 5%;
   }
   .display {
-    font-family: 'Orbitron', serif;
-    /* grid-area: display; */
     margin: 0 !important;
     width: 100%;
     @media (max-width: 500px) {
@@ -23,6 +26,7 @@ export const CalculatorStyles = styled.div`
     }
     h1 {
       font-size: 4rem;
+      font-weight: 400;
       color: white;
       text-align: center;
       @media (max-width: 500px) {
@@ -31,7 +35,6 @@ export const CalculatorStyles = styled.div`
     }
   }
   .num-pad {
-    /* grid-area: numbers; */
     display: grid;
     grid-gap: 10px;
     grid-template-columns: repeat(4, 1fr);
@@ -46,7 +49,7 @@ export const CalculatorStyles = styled.div`
       width: 100%;
       height: 80px;
       border-radius: 20px;
-      border: 3px solid white;
+      border: 2px solid white;
       font-size: 2rem;
       color: #080708;
       font-family: 'Orbitron', serif;
@@ -76,7 +79,8 @@ export const DisplayStyles = styled.div`
   display: grid;
   grid-template-rows: 90px 50px;
   grid-template-columns: 1fr;
-  border: 4px solid white;
+  border: 2px solid white;
+  width: 450px;
   max-width: 700px;
   margin: 10px auto;
   align-items: center;
@@ -95,7 +99,7 @@ export const DisplayStyles = styled.div`
     font-size: 2.5rem;
     margin: 0;
     text-align: right;
-    border-bottom: 4px solid white;
+    border-bottom: 2px solid white;
     padding: 15px 20px;
     @media (max-width: 500px) {
       font-size: 1.5rem;
